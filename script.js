a804d92be448f46f60218d55df5012cc2333aad4
Joseph Kelly <jkelly2191@gmail.com>
2020-10-17 01:35:12 -0500

    update readme, add images folder with image update script.js

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// TODO write a function that generates the password
+// TODO create a var to hold numbers, lowercase letters, uppercase letters and special characters
+// TODO create a variable to hold the password length
+// TODO prompt "How many characters would you like your password to contain?"
+//  TODO Confirm "Click Ok to confirm including special characters"
+//  TODO Confirm "Click Ok to confirm including numeric characters"
+//  TODO Confirm "Click Ok to confirm including lowercase characters"
+//  TODO Confirm "Click Ok to confirm including uppercase characters"
+
+
+
+var passwordLength = "";
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChar = "0123456789"
+var specialChar = "!@#$%^&*()[]\?/.,~`"
+
+
+
+function passwordGenerator(len) {
+  var length =
+}
