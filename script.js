c5e9961acc99475d71f2884991709250e785b4f9
Joseph Kelly <jkelly2191@gmail.com>
2020-10-17 22:34:56 -0500

    update script.js (working code)

--- script.js
+++ script.js
@@ -10,12 +10,22 @@ var pLength = "";
 var passwordCharSet = "";
 
 
-
-
+// function that writes password
 function writePassword() {
 
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+function generatePassword() {
+
   // password length set by user
-  var lengthInput = prompt("How many characters would you like your password to contain?");
+  var pLength  = prompt("How many characters would you like your password to contain?");
+
 
   // Boolean Variables asking for user input
   var numericInput = confirm("Click Ok to confirm including numeric characters");
@@ -23,6 +33,7 @@ function writePassword() {
   var upperInput = confirm("Click Ok to confirm including uppercase characters");
   var specialInput = confirm("Click Ok to confirm including special characters");
 
+  // use conditionals to set password criteria
   if (numericInput) {
     passwordCharSet += numericChar;
   }
@@ -34,10 +45,16 @@ function writePassword() {
   }
   if (specialInput) {
     passwordCharSet += specialChar;
+
     console.log(passwordCharSet)
   }
-
+  // empty string to fill random password
+  var randomPassword = "";
+  // for loop generates random characters within selected criteria 
+  for (var i = 0; i < pLength; i++) {
+    randomPassword += passwordCharSet.charAt(Math.floor(Math.random() *passwordCharSet.length));
+  }
+  return randomPassword;
 }
-
 // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
